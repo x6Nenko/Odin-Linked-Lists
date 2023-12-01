f22702eab4efb69a6f9be299a5303791315cfb06
Max I <clickkava@gmail.com>
2023-12-01 18:29:20 +0200

    Add the rest of functions:
    contains(value) - Prints true if the passed in value is in the list and
    otherwise returns false
    find(value) - Prints the index of the node containing value, or null if
    not found
    insertAt(value, index) - Insert a new node with the provided value at
    the given index
    removeAt(index) - Remove the node at the given index

--- index.js
+++ index.js
@@ -119,6 +119,135 @@ class LinkedList {
         previousNode.nextNode = null;
         this.tail = previousNode;
     };
+
+    // Print true if the passed in value is in the list and otherwise returns false.
+    contains(value) {
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (value === currentNode.value) {
+                console.log(true);
+                return;
+            };
+
+            currentNode = currentNode.nextNode;
+        };
+
+        console.log(false);
+        return;
+    };
+
+    // Print the index of the node containing value, or null if not found.
+    find(value) {
+        let currentNode = this.head;
+        let currentIndex = 0;
+
+        while (currentNode) {
+            if (value === currentNode.value) {
+                console.log(currentIndex);
+                return;
+            };
+
+            currentNode = currentNode.nextNode;
+            currentIndex++;
+        };
+
+        console.log(null);
+        return;
+    };
+
+    // Insert a new node with the provided value at the given index.
+    insertAt(value, index) {
+        if (index < 0) {
+            console.log("Index should be positive.");
+            return;
+        };
+
+        const newNode = new Node(value);
+
+        if (index === 0) {
+            // Insert the node at the beginning
+            newNode.nextNode = this.head;
+            this.head = newNode;
+    
+            if (!this.tail) {
+                // If the list was empty, update the tail
+                this.tail = newNode;
+            };
+    
+            return;
+        };
+
+        let currentNode = this.head;
+        let previousNode = null;
+        let currentIndex = 0;
+
+        while (currentNode && currentIndex < index) {
+            previousNode = currentNode;
+            currentNode = currentNode.nextNode;
+            currentIndex++;
+        };
+
+        if (currentIndex === index) {
+            // Insert at the given index
+            newNode.nextNode = currentNode;
+            previousNode.nextNode = newNode;
+
+            if (!currentNode) {
+                // If it was the end of list, update the tail
+                this.tail = newNode;
+            };
+        } else {
+            console.log("There is no such index in the list.");
+        };
+    };
+
+    // Remove the node at the given index.
+    removeAt(index) {
+        if (index < 0) {
+            console.log("Index should be positive.");
+            return;
+        };
+
+        if (!this.head) {
+            console.log("The linked list is empty.");
+            return;
+        };
+
+        if (index === 0) {
+            // Remove the node at the beginning
+            this.head = this.head.nextNode;
+    
+            if (!this.tail) {
+                // If the list becomes empty, update the tail
+                this.tail = null;
+            };
+    
+            return;
+        };
+
+        let currentNode = this.head;
+        let previousNode = null;
+        let currentIndex = 0;
+
+        while (currentNode && currentIndex < index) {
+            previousNode = currentNode;
+            currentNode = currentNode.nextNode;
+            currentIndex++;
+        };
+
+        if (currentIndex === index) {
+            // Remove the node at the given index
+            previousNode.nextNode = currentNode.nextNode;
+
+            if (!currentNode.nextNode) {
+                // If it was the end of list, update the tail
+                this.tail = previousNode;
+            };
+        } else {
+            console.log("There is no such index in the list.");
+        };
+    };
   
     // Print the values of all nodes in the list
     print() {
@@ -150,4 +279,19 @@ linkedList.at(1); // Output: 2
 linkedList.pop();
 linkedList.print(); // Output: 1 -> 2
 linkedList.append(3);
-  
+
+linkedList.contains(2); // Output: true
+linkedList.contains(4); // Output: false
+
+linkedList.find(2); // Output: 1
+linkedList.find(4); // Output: null
+
+linkedList.insertAt(5, 2); // Success
+linkedList.insertAt(5, -1); // Output: Index should be positive.
+linkedList.insertAt(5, 8); // There is no such index in the list.
+linkedList.print();
+
+linkedList.removeAt(-1); // Output: Index should be positive.
+linkedList.removeAt(8); // There is no such index in the list.
+linkedList.removeAt(2); // Success
+linkedList.print();
